7d7771f412fe220f89e891796b81e8955b961f13
Dylan Harper <dylanharper4@icloud.com>
2022-03-07 10:37:50 -0800

    Add some sad path testing

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Books Request' do
     quantity = 5
     get "/api/v1/book-search?location=#{location}&quantity=#{quantity}"
     expect(response).to be_successful
+    expect(response.status).to eq(200)
 
     books = JSON.parse(response.body, symbolize_names: true)[:data]
     expect(books[:type]).to eq('books')
@@ -20,6 +21,43 @@ RSpec.describe 'Books Request' do
       expect(i).to have_key(:isbn)
       expect(i).to have_key(:title)
       expect(i).to have_key(:publisher)
+    end
+  end
+
+  describe 'sad paths' do
+    it 'cannot enter an integer smaller than one', :vcr do
+      location = 'denver,co'
+      quantity = 0
+      get "/api/v1/book-search?location=#{location}&quantity=#{quantity}"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:error]).to eq("Quantity must be an integer above 0")
+      expect(message[:status]).to eq("400 Bad Request")
+    end
+
+    it 'cannot leave location blank', :vcr do
+      location = ''
+      quantity = 5
+      get "/api/v1/book-search?location=#{location}&quantity=#{quantity}"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:error]).to eq("Must enter a valid location and quantity in request parameters")
+      expect(message[:status]).to eq("400 Bad Request")
+    end
+
+    it 'cannot leave location or quantity empty', :vcr do
+      quantity = 5
+      get "/api/v1/book-search?quantity=#{quantity}"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:error]).to eq("Must enter a valid location and quantity in request parameters")
+      expect(message[:status]).to eq("400 Bad Request")
     end
   end
 
