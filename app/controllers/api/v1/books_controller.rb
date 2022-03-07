727285958a5b5ef2294a89ab2abe04dcd5a27c5e
Dylan Harper <dylanharper4@icloud.com>
2022-03-07 10:41:42 -0800

    Final commit?

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::BooksController < ApplicationController
 
   def index
-    if params[:quantity].to_i < 1
-      return render json: { error: 'Quantity must be an integer above 0', status: "400 Bad Request" }, status: 400
-    end
-
     if params[:location] == nil || params[:quantity] == nil || params[:location].blank?
       return render json: { error: 'Must enter a valid location and quantity in request parameters', status: "400 Bad Request" }, status: 400
+    end
+
+    if params[:quantity].to_i < 1
+      return render json: { error: 'Quantity must be an integer above 0', status: "400 Bad Request" }, status: 400
     end
 
     results = BookFacade.get_results_for(params[:location], params[:quantity])
