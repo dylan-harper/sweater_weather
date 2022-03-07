0c98d0ac864c41d6efdc308da14364410154a438
Dylan Harper <dylanharper4@icloud.com>
2022-03-06 19:35:38 -0800

    Fix array index

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,7 @@
 class ForecastFacade
 
   def self.get_forecast_for(location)
-    coords = CoordinateService.get_city_coordinates(location)[:locations][1][:latLng]
+    coords = CoordinateService.get_city_coordinates(location)[:locations][0][:latLng]
     data = ForecastService.get_forecast_for(coords)
     Forecast.new(data)
   end
