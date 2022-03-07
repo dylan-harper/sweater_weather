3e7e3af99b0826a2fa8df49db9d71abbf230cb79
Dylan Harper <dylanharper4@icloud.com>
2022-03-07 09:58:36 -0800

    Taking a pom

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Books Request' do
 
-  it 'retrieves books and forecast based on search' do
-    get "/api/v1/book-search?location=denver,co&quantity=5"
+  it 'retrieves books and forecast based on search', :vcr do
+    location = 'denver,co'
+    quantity = 5
+    get "/api/v1/book-search?location=#{location}&quantity=#{quantity}"
     expect(response).to be_successful
 
-    books = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
+    books = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(books[:type]).to eq('books')
+    expect(books[:attributes][:destination]).to eq(location)
+    expect(books[:attributes][:forecast]).to have_key(:summary)
+    expect(books[:attributes][:forecast]).to have_key(:temperature)
+    expect(books[:attributes][:total_books_found]).to be_an Integer
+    expect(books[:attributes][:books].length).to eq(quantity)
+
+    books[:attributes][:books].each do |i|
+      expect(i).to have_key(:isbn)
+      expect(i).to have_key(:title)
+      expect(i).to have_key(:publisher)
+    end
   end
 
 end
