81aef5dca95477d335657a74a8784020328b04c5
Dylan Harper <dylanharper4@icloud.com>
2022-03-07 08:28:06 -0800

    Build out book service

--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -1,4 +1,23 @@
 class BookService
 
+  def self.search(location, quantity)
+    loc = location.split(',')
+    get_data("/search.json?q=#{loc[0]}+#{loc[1]}&limit=#{quantity}")
+  end
+
+private
+
+  def self.conn
+    Faraday.new(url:"https://api.openweathermap.org") do |faraday|
+      #params as needed
+    end
+  end
+
+  def self.get_data(url)
+    response = conn.get(url) do |faraday|
+      #params as needed
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
 end
--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe BookService do
     describe '::search(query)' do
       it 'can search books given parameters' do
         location = 'denver,co'
-        results = BookService.search(location)
+        quantity = 10
+        results = BookService.search(location, quantity)
 
         expect(results).to be_a Hash
       end
